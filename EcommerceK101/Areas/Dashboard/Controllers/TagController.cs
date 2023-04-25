6948b89c8467abdddfaed87864d27e6c043cdfa4
MrAzimzadeh <mehemmedezimzase@gmail.com>
2023-04-25 10:33:47 +0400

    Add Tag Crud

---
+++ EcommerceK101/Areas/Dashboard/Controllers/TagController.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using EcommerceK101.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApp.Data;
+
+namespace EcommerceK101.Areas.Dashboard.Controllers
+{
+    [Area(nameof(Dashboard))]
+    public class TagController : Controller
+    {
+        private readonly ILogger<TagController> _logger;
+        private readonly AppDbContext _context;
+
+
+        public TagController(ILogger<TagController> logger, AppDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var tags = _context.Tags.ToList();
+            return View(tags);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Tag tag)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View(tag);
+            }
+            var tagExsist = _context.Tags.FirstOrDefault(x => x.Name == tag.Name);
+            if (tagExsist != null)
+            {
+                ViewBag.tagExist = "this Tag    is exist ";
+                return View();
+            }
+            _context.Tags.Add(tag);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Edit(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            var update = _context.Tags.FirstOrDefault(x => x.Id == id);
+            return View(update);
+
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Tag tag)
+        {
+            var findTags = _context.Tags.FirstOrDefault(x => x.Name == tag.Name);
+            if (findTags != null)
+            {
+                ViewBag.TagExist = "this Tag is exist ";
+                return View();
+            }
+            var edit = _context.Tags.Update(findTags);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            var delete = _context.Tags.FirstOrDefault(x => x.Id == id);
+            return View(delete);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Tag tag)
+        {
+            _context.Tags.Remove(tag);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
